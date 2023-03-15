3499593f00ec87216f764bb05c85c2fafb5c7fc0
Travis-Simmons <travisthomassimmons@gmail.com>
2023-03-15 15:54:42 -0400

    updated model downloading

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ echo "Container Started"
 export PYTHONUNBUFFERED=1
 source /workspace/stable-diffusion-webui/venv/bin/activate
 cd /workspace/stable-diffusion-webui
-wget -O model.safetensors $1
 echo "starting api"
 python webui.py --port 3000 --nowebui --api --xformers --ckpt /model.safetensors &
 cd /
